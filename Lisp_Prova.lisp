024999e027a42947fbdc500aa7fd4b1ed76919d8
Perego98 <d.perego23@campus.unimib.it>
2019-02-03 16:23:24 +0100

    Metodo def-class aggiornato, aggiunta la sezione iniziale che lo collega alla hashtable, e controllo della sua precedemte esistenza. Se non è gia presente. lo aggiunge. se è gia presente ritorna un messaggio di errore

--- Lisp_Prova.lisp
+++ Lisp_Prova.lisp
@@ -1,12 +1,19 @@
+;creazione della association list
 (defparameter *classes-specs* (make-hash-table))
 
+;definizioni metodi getter and setter per l'association list
 (defun add-class-spec (name class-spec)
   (setf (gethash name *classes-specs*) class-spec))
 (defun get-class-spec (name)
   (gethash name *classes-specs*))
 
+
+;;! Riscontrato errore usando defparameter, alla seconda creazione, 
+;;! usando def-class diceva che era già presete un valore, quello creato in precedenza
+;definizione del metodo defun-class
+;come prima cosa deve creare un link sulla hashtable basandomi sul nomClasse
+;controllo che non esista già una classe persona
 (defun def-class (nomeClasse superClassi campi)
-  (defparameter 'nomeClasse nomeClasse) 
-  (defparameter 'superClassi superClassi)
-  (defparameter 'campi campi))
-  
+ ; (print nomeClasse)
+  (cond ((not (get-class-spec nomeClasse)) (add-class-spec nomeClasse *classes-specs*))
+        (T (print "errore, classe gia presente"))))
