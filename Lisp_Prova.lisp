5fb3ff05412b160d043f8bacb6f5e4a9b845cced
fornaroale <fornaroale@outlook.com>
2019-02-04 15:19:01 +0100

    Inserita funzione new. Bisogna implementare nome istanza: al momento viene creata solo un istanza con lo stesso nome della classe e gli vengono associati gli attributi correttamente. Se viene creato s1 ed s2 con stesso argomento, la funzione restituisce errore dicendo che l'oggetto esiste già (in questo caso la verifica viene fatta sul nome della classe, bisogna invece spostarla sul nome dell'istanza - che non ho ancora ben capito come "prelevare").

--- Lisp_Prova.lisp
+++ Lisp_Prova.lisp
@@ -1,5 +1,7 @@
+
 ;creazione della association list
 (defparameter *classes-specs* (make-hash-table))
+
 
 ;definizioni metodi getter and setter per l'association list
 (defun add-class-spec (name class-spec)
@@ -9,12 +11,29 @@
 
 
 ;;! Riscontrato errore usando defparameter, alla seconda creazione, 
-;;! usando def-class diceva che era gi‡ presete un valore, quello creato in precedenza
+;;! usando def-class diceva che era gi√† presete un valore, quello creato in precedenza
 ;definizione del metodo defun-class
 ;come prima cosa deve creare un link sulla hashtable basandomi sul nomClasse
-;controllo che non esista gi‡ una classe persona
+;controllo che non esista gi√† una classe persona
 (defun def-class (nomeClasse superClassi campi)
- ; (print nomeClasse)
+  ;(print nomeClasse)
   (cond ((not (get-class-spec nomeClasse)) (add-class-spec nomeClasse campi))
         (T (print "errore, classe gia presente"))))
+
+
+; Funzione new: 
+(defun new (nomeClasse &rest campi)
+  ; verifico che non esista gia' il nome
+  (cond ((not (symbol-plist nomeClasse))
+	; qui devo verificare che ogni campo passato nel simbolo "campi" esista
+	; --------- ... ---------
+	; il primo campo della property list sara' nome della classe di appartenenza
+	;(setf (get nomeClasse ('name)) (nomeClasse)
+	; associo attributi all'oggetto il cui simbolo viene passato per argomento
+	(let ((contCampi 0))
+    (loop while (< contCampi (length campi)) do
+      (setf (get nomeClasse (nth contCampi campi)) (nth (+ contCampi 1) campi))
+      (incf contCampi 2)))
+   (symbol-plist nomeClasse))
+   (T (print "ERRORE, ISTANZA GIA' PRESENTE!"))))
 
