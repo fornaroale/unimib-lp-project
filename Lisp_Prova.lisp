26e49e8758d0da26716a0ba0cf68709dfb3dc34e
Perego98 <d.perego23@campus.unimib.it>
2019-02-06 16:17:54 +0100

    miglioramento def-class

--- Lisp_Prova.lisp
+++ Lisp_Prova.lisp
@@ -1,28 +1,41 @@
 
-;creazione della association list
+; creazione della association list
 (defparameter *classes-specs* (make-hash-table))
 
 
-;definizioni metodi getter and setter per l'association list
+; definizioni metodi getter and setter per l'association list
 (defun add-class-spec (name class-spec)
   (setf (gethash name *classes-specs*) class-spec))
 (defun get-class-spec (name)
   (gethash name *classes-specs*))
 
 
-;;! Riscontrato errore usando defparameter, alla seconda creazione, 
-;;! usando def-class diceva che era già presete un valore, quello creato in precedenza
-;definizione del metodo defun-class
-;come prima cosa deve creare un link sulla hashtable basandomi sul nomClasse
+; definizione del metodo defun-class
+; come prima cosa deve creare un link sulla hashtable 
+; basandomi sul nomClasse
 ;controllo che non esista già una classe persona
-(defun def-class (nomeClasse superClassi &rest campi)
-  ;(print nomeClasse)
-  (cond ((not (get-class-spec nomeClasse)) 
-         (add-class-spec nomeClasse campi)
-         (write nomeClasse))         
-        (T (print "errore, classe gia presente")))
-  )
-;funzione che mi risolve i parents
+(defun def-class (class-name parents &rest slot-value)
+  ;controlli
+  (cond ((get-class-spec class-name) 
+         (print "errore, classe gia presente"))         
+        (T (progn
+			(add-class-spec class-name
+				;(list class-name parents slot-value)
+				(list class-name parents 
+					(fondi-attributi parents slot-value)
+				)
+		)
+		
+		class-name;
+		)
+		
+  ))
+; funzione che mi risolve i parents e attributi
+(defun fondi-attributi (l1 l2)
+
+
+
+)
 
 
 
