f71985f726367e81097b7bbab276e542f4a08ad9
fornaroale <fornaroale@outlook.com>
2019-02-06 19:11:01 +0100

    Prima modifica alla funzione new (da terminare)

--- Lisp_Prova.lisp
+++ Lisp_Prova.lisp
@@ -20,16 +20,16 @@
          (print "errore, classe gia presente"))         
         (T (progn
 			(add-class-spec class-name
-				;(list class-name parents slot-value)
-				(list class-name parents 
-					(fondi-attributi parents slot-value)
-				)
+				(list class-name parents slot-value)
+				;(list class-name parents 
+				;	(fondi-attributi parents slot-value)
+				;)
 		)
 		
 		class-name;
 		)
 		
-  ))
+  )))
 ; funzione che mi risolve i parents e attributi
 (defun fondi-attributi (l1 l2)
 
@@ -38,26 +38,43 @@
 )
 
 
-
-
-
-; Funzione new: 
-(defun new (nomeClasse &rest campi)
-  ; verifico che non esista gia' il nome
-  (cond ((not (symbol-plist nomeClasse))
-	; qui devo verificare che ogni campo passato nel simbolo "campi" esista
-	; --------- ... ---------
-	; il primo campo della property list sara' nome della classe di appartenenza
-	;(setf (get nomeClasse ('name)) (nomeClasse)
-	; associo attributi all'oggetto il cui simbolo viene passato per argomento
-	(let ((contCampi 0))
-    (loop while (< contCampi (length campi)) do
-      (setf (get nomeClasse (nth contCampi campi)) (nth (+ contCampi 1) campi))
-      (incf contCampi 2)))
-   (symbol-plist nomeClasse))
-   (T (print "ERRORE, ISTANZA GIA' PRESENTE!"))))
-
-;funzione ricorsiva che 
-(defun scorri-attributi (def-class n lungRagg nomeAttrbuto newValore)
-  
+; Funzione new: ritorna lista contenente valori di istanza
+(defun new (class-name &rest slot) ;<-- slot: lista di attributi di classe
+  (cond ((get-class-spec class-name)  ; verifico esistenza classe
+    ; copio attributi classe (e parenti) in istanza e rimpiazzo valori
+    ; default con valori istanza:
+    (list 'oolinst (checkSlot (get-class-spec class-name) slot 0))
+    ;(list 'oolinst class-name (get-class-spec class-name))
+    )  ; ritorno istanza
+    (T (print "Errore: classe inesistente!")))
+;	(let ((contArgomenti 0))
 )
+
+
+; Funzione checkSlot: verifica esistenza nomi slot istanza, e sostituisce
+;                     valori default classe con valori istanza
+; Devo modificare la instanceList (l'istanza) con i valori nuovi
+; Per ogni slot-name in slot, scorro instanceList, e se lo trovo, sostituisco
+; lo slot-value (posizione successiva ad esso) nella lista instanceList
+(defun checkSlot (instanceList slot slotCount)
+  (cond ((< slotCount (length slot)
+    ; lista slot: in pos. slotCount ho il nome dello slot, in pos.
+    ; slotCount+1 ho il relativo valore
+    (setInstVal (nth 2 instanceList) 
+                (nth slotCount slot) 
+                (nth (+ 1 slotCount) slot)
+                2) ; devo partire dalla pos. 2
+    ; proseguo con 'attributo' successivo
+    (checkSlot instanceList slot (+ 2 slotCount))))))
+
+
+; Funzione setInstVal: sostituisce il val. default con val. istanza
+(defun setInstVal (instanceList slot-name slot-value contSlotInstance)
+  (cond ((equal (nth contSlotInstance instanceList) slot-name)
+    ; sostituisco slot-value con valore nuovo
+    (setf (nth (+ 1 contSlotInstance) instanceList) slot-value)
+    ; altrimenti proseguo nella ricerca (ammesso che cont < length)
+    (T (cond ((< contSlotInstance (length instanceList))
+      (setInstVal instanceList slot-name slot-value (+ 2 contSlotInstance))
+       )
+)))))
