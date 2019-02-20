aa13c55cef0bf4557111cba7f31182e259806a78
Perego98 <d.perego23@campus.unimib.it>
2019-02-20 10:59:26 +0100

    non riesco ad usare correttamente assertz nel progetto

--- Prolog_prova.pl
+++ Prolog_prova.pl
@@ -1,10 +1,11 @@
 %% -*- Mode: Prolog -*-
-def_class(NomeClasse, Parents, SlotValues) :-
-    verifica(NomeClasse, Parents, SlotValues),
-    assegna(NomeClasse, Parents, SlotValues).
+def_class(NomeClasse, [], SlotValues) :-
+    verifica(NomeClasse, [], SlotValues),
+    assegna(NomeClasse, [], SlotValues).
 
 verifica(X, Y, Z) :-
     nonvar(X), nonvar(Y), nonvar(Z).
 
-assegna(X, Y, Z) :-
-    assertz(X, (Y, Z)).
+assegna(X, [], Z) :-
+    assertz((X(Z))).
+
